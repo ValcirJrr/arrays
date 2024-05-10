ed619161b5a0b887847777940edc282c643aa5f0
Junior <vbalbino@nttdata.com>
2024-05-10 18:32:52 -0300

    extract menu to a variable

--- src/Main.java
+++ src/Main.java
@@ -8,17 +8,20 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         while (true){
-            System.out.println("========== Menu ===========\n" +
-                    "1 - Add Item" +
-                    "2 - Price Item" +
-                    "3 - List Items" +
-                    "4 - New Order" +
-                    "5 - Order Item" +
-                    "6 - List Orders" +
-                    "7 - Finish Order");
-
-            int selectedOption = scanner.nextInt();
+            int selectedOption = showMenu(scanner);
             if(selectedOption == 0) break;
         }
+    }
+
+    private static int showMenu(Scanner scanner) {
+        System.out.println("========== Menu ===========\n" +
+                "1 - Add Item" +
+                "2 - Price Item" +
+                "3 - List Items" +
+                "4 - New Order" +
+                "5 - Order Item" +
+                "6 - List Orders" +
+                "7 - Finish Order");
+        return scanner.nextInt();
     }
 }
