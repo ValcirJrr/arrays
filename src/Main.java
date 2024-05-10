62cb5f47b95f788aeb25967991bc77109492c898
Junior <vbalbino@nttdata.com>
2024-05-10 18:45:38 -0300

    create arrays and matrix to represent our objects

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,30 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        /*
+        * The idea between the Arrays creation is to the items and prices represents
+        * something like a restaurant menu, where the same position will refer to the same item and price
+        * for example, if at items[1] = "coke" and prices[1] = 5.00, it means that in our restaurant menu
+        * has a coke that costs $5.00
+        *
+        * The position 0 will not be used, because the orders matrix will be always fill blank spaces as 0
+        * so, it represents that has no item.
+        *
+        * */
+        String[] items = new String[1];
+        double[] prices = new double[1];
+        /*
+        *  the orders matrix will have lines that represents orders and columns that represents item
+        *  so
+        *  1 2 0 0
+        *  3 1 1 4
+        *  2 0 0 0
+        *
+        * in this case we have three orders, the first one with two items, second with four items and the third
+        * with just an item
+        * */
+        int[][] orders = new int[0][0];
+
         while (true){
             int selectedOption = showMenu(scanner);
             if(selectedOption == 0) break;
