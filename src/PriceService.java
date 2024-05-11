d117b0eb52f47e85ef1874881f13b62d3473bd40
Junior <vbalbino@nttdata.com>
2024-05-11 13:15:43 -0300

    create a function to append a price value to prices array

--- src/PriceService.java
+++ src/PriceService.java
@@ -1,11 +1,19 @@
 package src;
 
+import java.util.Scanner;
+import java.util.function.Function;
+
 public class PriceService {
+
     public double[] increaseArray(double[] prices){
         return increaseLengthAndCopyArray(prices);
     }
 
-    private static double[] increaseLengthAndCopyArray(double[] prices) {
+    public void priceAnItem(double[] prices, int itemID, double itemPrice){
+        prices[itemID] = itemPrice;
+    }
+
+    private double[] increaseLengthAndCopyArray(double[] prices) {
         double[] arrayToIncrease = new double[prices.length + 1];
         System.arraycopy(prices, 0, arrayToIncrease, 0, prices.length);
         return arrayToIncrease;
